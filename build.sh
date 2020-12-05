bac1c91ee5449c72e8d33fdf3f648c93e1e6e031
Akshit Jain <jaina488@gmail.com>
2020-12-05 13:34:06 +0530

    lib dir ignore

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 source ~/repos/ros_noetic/install_isolated/setup.bash
 colcon build \
 --cmake-args "-Wno-dev -Wno-deprecated" \
---packages-select behavior_selector
+--packages-up-to faster
 # --packages-up-to faster
 # --symlink-install \
 # --continue-on-error \
